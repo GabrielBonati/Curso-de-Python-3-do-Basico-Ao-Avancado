847368aace88fc7651a663ba2d7459801a11469c
Gabriel <gabrielbonatidias@hotmail.com>
2021-11-24 20:53:55 -0300

    Criação de geradores e validadores de cpf

---
+++ modulo1_basico/exs_finais/validador_cpf.py
@@ -0,0 +1,28 @@
+# Validador de Cpf
+while True:
+    #cpf = '16899535002'
+    cpf = input('Digite um Cpf:')
+    novo_cpf = cpf[:-2]
+    reverse = 10
+    total = 0
+
+    for index in range(19):
+        if index > 8:
+            index -= 9
+
+        total += int(novo_cpf[index]) * reverse
+
+        reverse -= 1
+        if reverse < 2:
+            reverse = 11
+            d = 11 - (total % 11)
+
+            if d > 9:
+                d = 0
+            total = 0
+            novo_cpf += str(d)
+
+    if cpf == novo_cpf:
+        print('Válido')
+    else:
+        print('Inválido')
